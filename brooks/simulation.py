2e00bfb64f3bfe019dc8a25048904da33ec0321e
Robert Smallshire <robert@smallshire.org.uk>
2015-09-28 16:27:05 +0200

    Fixed loop termination off-by-one blunder revealed by using days rather than seconds.

--- brooks/simulation.py
+++ brooks/simulation.py
@@ -1,5 +1,4 @@
 from brooks.state import State
-from brooks.util import cardinal
 
 
 def simulate(schedule):
@@ -17,16 +16,14 @@ def simulate(schedule):
     state = State(**args)
     step_number = 0
     elapsed_time_seconds = 0
-    while True:
+    while not schedule.is_complete(step_number, elapsed_time_seconds, state):
         state = schedule.intervene(step_number, elapsed_time_seconds, state)
         state = step(step_number, elapsed_time_seconds, state)
-        if schedule.is_complete(step_number, elapsed_time_seconds, state):
-            break
         step_number += 1
         elapsed_time_seconds += state.step_duration_days
     state = schedule.complete(step_number, elapsed_time_seconds, state)
-    print("number of steps: {}".format(cardinal(step_number)))
-    print("elapsed time: {} s".format(elapsed_time_seconds))
+    print("number of steps: {}".format(step_number))
+    print("elapsed time: {} days".format(elapsed_time_seconds))
     print(state)
 
 
