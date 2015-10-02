0751c3d9a2ac8ff8ed4f32e891a44b46b60ce73b
Robert Smallshire <robert@smallshire.org.uk>
2015-10-02 13:23:09 +0200

    A more specific name for the quadratic overhead model. Force zero overhead for personnel of one. Adds a Gompertz function based overhead model.

--- brooks/communication.py
+++ brooks/communication.py
@@ -1,15 +1,17 @@
 """Functions for modelling communications overhead."""
 
-from math import factorial as fac
+from math import exp
 
-def overhead_proportion(num_personnel):
+def quadratic_overhead_proportion(num_personnel):
     """The proportion of time spent on communication overhead.
 
     Abdel-Hamid model.
     """
-    if not (0 <= num_personnel <= 31):
+    if not (0 <= num_personnel < 31):
         raise ValueError("Communication overhead proportion personnel "
                          "number {} out of range".format(num_personnel))
+    if num_personnel == 1:
+        return 0
     return ((num_personnel / 4.082)**2) / 100.0
 
 
@@ -21,7 +23,15 @@ def partitioned_overhead_proportion(num_personnel, max_team_size):
     """
     num_partitions = num_personnel / min(max(1, num_personnel), max_team_size)
     num_personnel_per_partition = num_personnel / num_partitions
-    intra_team_overhead = overhead_proportion(num_personnel_per_partition)
-    inter_team_overhead = 10 * overhead_proportion(num_partitions)
+    intra_team_overhead = quadratic_overhead_proportion(num_personnel_per_partition)
+    inter_team_overhead = 10 * quadratic_overhead_proportion(num_partitions)
 
     return intra_team_overhead + inter_team_overhead
+
+
+def gompertz_overhead_proportion(num_personnel):
+    """A Gompertz function which saturates at high numbers of personnel."""
+    a = 1.0       # These values are adjusted to closely
+    b = 5.0       # match the Abdel-Hamid curve in the
+    c = 1 / 14.2  # range 0 to 30
+    return (a * exp(-b * exp(-c * num_personnel))) - (a * exp(-b))
