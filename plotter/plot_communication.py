7aa4752bd51d8bde5ca6cb135775cfa3b898851d
Robert Smallshire <robert@smallshire.org.uk>
2015-10-03 10:37:26 +0200

    Adds a script for plotting the communication overhead functions.

---
+++ plotter/plot_communication.py
@@ -0,0 +1,34 @@
+import seaborn
+
+import matplotlib.pyplot as plt
+
+from brooks.communication import *
+
+def main():
+    qo = []
+    qr = range(31)
+    for i in qr:
+        try:
+            overhead = quadratic_overhead_proportion(i)
+        except ValueError:
+            overhead = 0
+        qo.append(overhead)
+
+    go = []
+    gr = range(61)
+    for i in gr:
+        go.append(gompertz_overhead_proportion(i))
+
+
+    plt.figure(figsize=(8, 4.5), dpi=144)
+    qp, = plt.plot(qr, qo, label='Abdel-Hamid (Quadratic)')
+    gp, = plt.plot(gr, go, label='Smallshire (Gompertz)')
+    plt.xlabel('Number of personnel')
+    plt.ylabel('Communication overhead')
+    plt.legend(handles=[qp, gp], loc=4)
+    plt.gcf().savefig("plots/communication.pdf", dpi=144)
+
+
+if __name__ == '__main__':
+    main()
+
