4598c9d25e8117670ead11a8d33ec7589581b505
Robert Smallshire <robert@smallshire.org.uk>
2015-10-26 21:04:30 +0100

    A test schedule for issue #3.

---
+++ schedules/schedule_issue_3.py
@@ -0,0 +1,39 @@
+import brooks.communication
+
+from brooks.state import State
+
+def initial():
+    return State(
+        step_duration_days=1,
+        num_function_points_requirements=500,
+        num_function_points_developed=0,
+        num_new_personnel=20,
+        num_experienced_personnel=0,
+        personnel_allocation_rate=0,
+        personnel_assimilation_rate=0,
+        assimilation_delay_days=20,
+        nominal_productivity=0.1,
+        new_productivity_weight=0.8,
+        experienced_productivity_weight=1.2,
+        training_overhead_proportion=0.75,
+        communication_overhead_function=brooks.communication.no_overhead,
+        software_development_rate=None,
+        cumulative_person_days=0,
+    )
+
+
+def intervene(step_number, elapsed_time, state):
+    """Intervene in the current step before the main simulation step is executed."""
+    if elapsed_time == 110:
+        state.num_new_personnel += 10
+    return state
+
+
+def is_complete(step_number, elapsed_time_seconds, state):
+    return state.num_function_points_developed >= state.num_function_points_requirements
+
+
+def complete(step_number, elapsed_time_seconds, state):
+    state.software_development_rate = 0
+    return state
+
